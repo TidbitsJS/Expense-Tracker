7d6585b0f3e3a9d683d6d20a4437be9d3869462b
sujata <gunalesujata@gmail.com>
2020-08-09 23:37:22 +0530

    Updates

---
+++ precache-manifest.99fb9eb0433c008b515ab3c49c553a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3feece60f8000ecfb38fd46b2aec9b",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "cbf967cb77ae105c91b0",
+    "url": "/Expense-Tracker/static/css/main.39e834d5.chunk.css"
+  },
+  {
+    "revision": "29aeaa6d784054a1683e",
+    "url": "/Expense-Tracker/static/js/2.eb9302d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.eb9302d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf967cb77ae105c91b0",
+    "url": "/Expense-Tracker/static/js/main.f201ebd7.chunk.js"
+  },
+  {
+    "revision": "b00816d2ec4e0511ccd5",
+    "url": "/Expense-Tracker/static/js/runtime-main.ae8039cb.js"
+  }
+]);
